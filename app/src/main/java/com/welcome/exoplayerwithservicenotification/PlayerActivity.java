319fd1bc402661d3c8bcdf055a51f57b41637c69
MNGsulochana <masulochana@gmail.com>
2021-01-10 00:04:47 +0530

    Created Exoplayer wrto activity life cycle

---
+++ app/src/main/java/com/welcome/exoplayerwithservicenotification/PlayerActivity.java
@@ -0,0 +1,136 @@
+package com.welcome.exoplayerwithservicenotification;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    PlayerView playView;
+    SimpleExoPlayer  exoPlayer;
+
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        playView=findViewById(R.id.play_view);
+    }
+
+    /**
+     * TO GET THE VIDEO LINKS GO TO PLAYLISTS SECTION
+     * https://github.com/google/ExoPlayer/blob/release-v2/demos/main/src/main/assets/media.exolist.json
+     *
+     * SAMPLE URLS
+     * https://html5demos.com/assets/dizzy.mp4
+     *         //http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4
+     *         https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
+     *
+     *  SAMPLE PROJECT
+     *         https://github.com/yusufcakmak/ExoPlayerSample
+     */
+    private void initializePlayer() {
+
+        exoPlayer=new SimpleExoPlayer.Builder(this).build();
+        playView.setPlayer(exoPlayer);
+        List<MediaSource> m=new ArrayList<>();
+        DefaultDataSourceFactory  dataSourceFactory = new DefaultDataSourceFactory(this, "sample");
+        MediaSource mediaSource= new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"));
+        for(int i=0; i<=5; i++)
+        {
+            m.add(i,mediaSource);
+            Log.d("sizelist1",""+m.get(i));
+        }
+        //exoPlayer.setMediaSource(mediaSource);//to play single content
+        exoPlayer.setMediaSources(m);
+        Log.d("sizelist",""+m.size());
+        exoPlayer.setPlayWhenReady(playWhenReady);
+        exoPlayer.seekTo(currentWindow, playbackPosition);
+        exoPlayer.prepare();
+        exoPlayer.play();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if(exoPlayer==null)
+            initializePlayer();
+    }
+
+    private void initializePlayer1() {
+        exoPlayer=new SimpleExoPlayer.Builder(this).build();
+        playView.setPlayer(exoPlayer);
+
+        MediaItem mediaItem=MediaItem.fromUri(Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"));
+        exoPlayer.setMediaItem(mediaItem);
+        exoPlayer.setPlayWhenReady(playWhenReady);
+        exoPlayer.seekTo(currentWindow, playbackPosition);
+        exoPlayer.prepare();
+        exoPlayer.play();
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(exoPlayer==null)
+            initializePlayer();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(exoPlayer==null)
+            initializePlayer();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+            releasePlayer();
+    }
+
+    private void releasePlayer() {
+        if (exoPlayer != null) {
+            playWhenReady = exoPlayer.getPlayWhenReady();
+            playbackPosition = exoPlayer.getCurrentPosition();
+            currentWindow = exoPlayer.getCurrentWindowIndex();
+            exoPlayer.release();
+            exoPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+            releasePlayer();
+
+    }
+}
